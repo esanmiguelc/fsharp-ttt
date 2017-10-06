96f2b2f6c58bbece2523c80bcabc1c5f78d01f76
esanmiguelc <esanmiguel@gmail.com>
2017-10-06 17:09:11 -0500

    Adds placing marks

--- Core.Tests/Tests.fs
+++ Core.Tests/Tests.fs
@@ -6,10 +6,67 @@ open Core.Board
 
 [<Fact>]
 let ``it returns an empty board`` () =
-    let board = {cells=[{value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}]}
-    Assert.Equal(board, emptyBoard)
+    let board: Board = Map.ofList [(0, None); (1, None); (2, None);
+                                   (3, None); (4, None); (5, None);
+                                   (6, None); (7, None); (8, None)]
+    Assert.Equal<Board>(board, emptyBoard 3)
 
 [<Fact>]
 let ``it fills the board with a marker`` () =
-    let board = {cells=[{value = Some "X"}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}]}
-    Assert.Equal(Ok board, fillBoard emptyBoard "X" 0) 
+    let board = Map.ofList [(0, Some X); (1, None); (2, None);
+                            (3, None); (4, None); (5, None);
+                            (6, None); (7, None); (8, None)]
+    Assert.Equal(Ok board, fillBoard (emptyBoard 3) 0)
+
+[<Fact>]
+let ``it fills the board with a second marker`` () =
+    let board = Map.ofList [(0, Some X); (1, None); (2, None);
+                             (3, None); (4, None); (5, None);
+                             (6, None); (7, None); (8, None)]
+    let expectedBoard = Map.ofList [(0, Some X); (1, Some O); (2, None);
+                            (3, None); (4, None); (5, None);
+                            (6, None); (7, None); (8, None)]
+
+    Assert.Equal(Ok expectedBoard, (fillBoard board 1))
+
+[<Fact>]
+let ``it returns invalid position if the position requested is not within the correct range`` () =
+    Assert.Equal(Error InvalidPosition, fillBoard (emptyBoard 3) -1)
+
+[<Fact>]
+let ``it returns position taken if the position requested is already taken by a player`` () =
+    let board = Map.ofList [(0, Some X); (1, None); (2, None);
+                            (3, None); (4, None); (5, None);
+                            (6, None); (7, None); (8, None)]
+    Assert.Equal(Error PositionTaken, fillBoard board  0)
+
+[<Fact>]
+let ``Determines when the game is over with a tie`` () =
+    Assert.Equal(1,2)
+
+// playGame (emptyBoard 3) 0
+// let playGame board position =
+//     match (fillBoard (emptyBoard 3) position) with
+//     (Ok newBoard) ->
+//         // playGame board getNewPositon
+//     Tie board ->
+        // print board
+        // print it's a tie!
+//      // do something with a tie
+//     Winner mark ->
+//         // do something with the winner
+//     (Error _) ->
+//      You messed up this way, fix it
+//      // deal with error
+
+// (fillBoard) |> isGameOver
+// whoWon
+// match (fillBoard oldBoard position) with
+//     (Ok board) ->
+//         // get next move
+//     Tie ->
+//      // do something with a tie
+//     Winner mark ->
+//         // do something with the winner
+//     (Error _) ->
+//      // deal with error
--- Core/Library.fs
+++ Core/Library.fs
@@ -1,21 +1,32 @@
 namespace Core
 
-open System
-
 module Board =
-    type Cell = { value:Option<string> }
-    type Board = { cells:list<Cell> }
+    type PlayerMark =
+    | X
+    | O
+    type Cell = Option<PlayerMark>
+    type Board = Map<int,Cell>
     type BoardError =
-    | InvalidLocation
+    | InvalidPosition
     | PositionTaken
 
-    let emptyBoard : Board =
-      {cells=[{value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}]}
+    let emptyBoard (size: int) : Board =
+      [0 .. (size * size - 1)] |> List.map (fun position -> (position,  None)) |> Map.ofList
+ 
+    let private isWithinRange(board: Board, position: int) : bool =
+      Map.exists (fun key v -> key = position) board
 
-    let fillBoard (board : Board) (mark : string) (position : int) : Result<Board, BoardError> =
-      try
-        let item = List.item position board.cells
-        Ok {cells=[{value = Some "X"}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}; {value = None}]}
-      with
-      | :? ArgumentException -> Error InvalidLocation
+    let private findPlayerMark (board: Board) =
+      let spacesTaken = Map.filter (fun _ v -> v <> None) board
+      let numSpacesTaken = Map.toList spacesTaken |> List.length
+      if numSpacesTaken % 2 = 0 then X else O
 
+    let fillBoard (board: Board) (position: int) : Result<Board, BoardError> =
+      if isWithinRange(board, position) then
+        match (Map.find position board) with
+          | None ->
+            Ok (board |> Map.add position (Some (findPlayerMark board)))
+          | _ ->
+            Error PositionTaken
+      else
+        Error InvalidPosition
